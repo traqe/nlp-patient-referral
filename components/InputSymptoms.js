6dcbb5a4fa8933acafb0372f126c16a740f3c28d
traqe <you@example.com>
2024-01-09 22:26:11 +0200

    added alert to submit button

--- components/InputSymptoms.js
+++ components/InputSymptoms.js
@@ -1,30 +1,40 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function InputSymptoms() {
 
-    const [symptoms, setSymptoms] = useState();
+    const [symptoms, setSymptoms] = useState('');
 
     const changeHandler = (val) => {
         setSymptoms(val);
     }
 
+    const submitButton = () => {
+        if(symptoms != '' ) {
+            console.log('this is where your NLP algorithm comes in');
+        }
+        else {
+            Alert.alert('Empty Input', 'You cannot submit empty symptom input', [{text: 'Ok', onPress: () => console.log('user passed no input')}]);
+        }
+    }
+
     return (
         <View>
             <TextInput 
-             placeholder='input symptoms'
+             placeholder="Input Patient's Symptoms"
              style={styles.input}
              onChangeText={changeHandler}
-             multiline
               />
+            <View style={styles.br}></View>
             <Text style={styles.summary}>
                 Summary: {symptoms}
             </Text>
+            
             <View style={styles.button}>
                 <Button 
                  title='Submit' 
                  color={'#fca'}
-                 onPress={() => {}} /> 
+                 onPress={submitButton} /> 
             </View>
         </View>
     )
@@ -37,11 +47,16 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         borderColor: '#fca',
         padding: 10,
+        marginTop: 80,
     },
     button: {
         margin: 55,
+        marginTop: 80,
     },
     summary: {
         padding: 20,
+        backgroundColor: '#fca',
+        marginTop: 15,
+        
       }
 })
